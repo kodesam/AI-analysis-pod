6da3490f483c4675160cd31bf73d0517066473c0
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2023-12-07 23:10:13 +0100

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -1,13 +1,19 @@
 import streamlit as st
 
 def analyze_pod_logs(logs):
-    # Analysis logic here
+    # Analysis logic
     # ...
+
+    analysis_result = {
+        "total_logs": len(logs.strip().split('\n')),
+        "sample_logs": logs.strip().split('\n')[:5],
+        # Add more analysis results as needed
+    }
 
     return analysis_result
 
 def main():
-    st.title("Pod Log Analysis")
+    st.title("Pod Log Analyzer")
 
     # Get the input pod logs from user input in Streamlit
     logs_input = st.text_area("Enter Pod Logs")
