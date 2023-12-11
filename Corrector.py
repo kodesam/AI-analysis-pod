9f100df0b5f75aa5737fb461c47805d1c0e6982e
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2023-12-11 19:40:01 +0100

    Update Corrector.py

--- Corrector.py
+++ Corrector.py
@@ -1,33 +1,37 @@
 import streamlit as st
 import yaml
 
-def correct_spacing(data):
+def correct_spacing(filename):
+    with open(filename, 'r') as file:
+        data = yaml.safe_load(file)
+    
     # Dump the corrected YAML content into a string
-    corrected_yaml = yaml.dump(data, sort_keys=False)
-
+    corrected_yaml = yaml.dump(data, default_flow_style=False)
+    
     # Return the corrected YAML content
     return corrected_yaml
 
 def main():
-    st.title("YAML Spacing Corrector")
+    st.title("Ansible Script YAML Spacing Corrector")
 
     # File upload widget
-    uploaded_file = st.sidebar.file_uploader("Upload YAML file", type=['yml', 'yaml'])
-
+    uploaded_file = st.sidebar.file_uploader("Upload Ansible Script YAML", type=['yml', 'yaml'])
+    
     if uploaded_file is not None:
         try:
-            # Read the uploaded YAML file
-            yaml_data = yaml.safe_load(uploaded_file)
-
+            # Saving uploaded YAML to a temporary file
+            with open('temp.yaml', 'wb') as file:
+                file.write(uploaded_file.read())
+            
             # Correct the spacing
-            corrected_yaml = correct_spacing(yaml_data)
-
+            corrected_yaml = correct_spacing('temp.yaml')
+            
             # Display the corrected YAML on the screen
             st.text_area("Corrected YAML", value=corrected_yaml, height=500)
         except yaml.YAMLError as e:
             st.error("Error parsing the YAML file. Please make sure it is a valid YAML file.")
     else:
-        st.sidebar.info("Please upload a YAML file.")
+        st.sidebar.info("Please upload an Ansible Script YAML file.")
 
 if __name__ == '__main__':
     main()
