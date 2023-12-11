1136df8985b7859186566eea9bde8d516011b608
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2023-12-11 19:44:57 +0100

    Update Corrector.py

--- Corrector.py
+++ Corrector.py
@@ -2,14 +2,17 @@ import streamlit as st
 import yaml
 
 def correct_spacing(filename):
-    with open(filename, 'r') as file:
-        data = yaml.safe_load(file)
-    
-    # Dump the corrected YAML content into a string
-    corrected_yaml = yaml.dump(data, default_flow_style=False)
-    
-    # Return the corrected YAML content
-    return corrected_yaml
+    try:
+        with open(filename, 'r') as file:
+            data = yaml.safe_load(file)
+        
+        # Dump the corrected YAML content into a string
+        corrected_yaml = yaml.dump(data, default_flow_style=False)
+        
+        # Return the corrected YAML content
+        return corrected_yaml
+    except yaml.YAMLError as e:
+        st.error("Error parsing the YAML file. Please make sure it is a valid YAML file.")
 
 def main():
     st.title("Ansible Script YAML Spacing Corrector")
@@ -18,18 +21,16 @@ def main():
     uploaded_file = st.sidebar.file_uploader("Upload Ansible Script YAML", type=['yml', 'yaml'])
     
     if uploaded_file is not None:
-        try:
-            # Saving uploaded YAML to a temporary file
-            with open('temp.yaml', 'wb') as file:
-                file.write(uploaded_file.read())
-            
-            # Correct the spacing
-            corrected_yaml = correct_spacing('temp.yaml')
-            
+        # Saving uploaded YAML to a temporary file
+        with open('temp.yaml', 'wb') as file:
+            file.write(uploaded_file.read())
+        
+        # Correct the spacing
+        corrected_yaml = correct_spacing('temp.yaml')
+        
+        if corrected_yaml:
             # Display the corrected YAML on the screen
             st.text_area("Corrected YAML", value=corrected_yaml, height=500)
-        except yaml.YAMLError as e:
-            st.error("Error parsing the YAML file. Please make sure it is a valid YAML file.")
     else:
         st.sidebar.info("Please upload an Ansible Script YAML file.")
 
