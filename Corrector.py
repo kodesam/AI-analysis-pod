b9349ccc92480bf0ff1db3c21558b7934324765d
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2023-12-11 19:25:01 +0100

    Update Corrector.py

--- Corrector.py
+++ Corrector.py
@@ -3,8 +3,8 @@ import yaml
 
 def correct_spacing(data):
     # Dump the corrected YAML content into a string
-    corrected_yaml = yaml.dump(data, default_flow_style=False)
-    
+    corrected_yaml = yaml.dump(data, sort_keys=False)
+
     # Return the corrected YAML content
     return corrected_yaml
 
@@ -13,16 +13,19 @@ def main():
 
     # File upload widget
     uploaded_file = st.sidebar.file_uploader("Upload YAML file", type=['yml', 'yaml'])
-    
+
     if uploaded_file is not None:
-        # Read the uploaded YAML file
-        yaml_data = yaml.safe_load(uploaded_file)
-        
-        # Correct the spacing
-        corrected_yaml = correct_spacing(yaml_data)
-        
-        # Display the corrected YAML on the screen
-        st.text_area("Corrected YAML", value=corrected_yaml, height=500)
+        try:
+            # Read the uploaded YAML file
+            yaml_data = yaml.safe_load(uploaded_file)
+
+            # Correct the spacing
+            corrected_yaml = correct_spacing(yaml_data)
+
+            # Display the corrected YAML on the screen
+            st.text_area("Corrected YAML", value=corrected_yaml, height=500)
+        except yaml.YAMLError as e:
+            st.error("Error parsing the YAML file. Please make sure it is a valid YAML file.")
     else:
         st.sidebar.info("Please upload a YAML file.")
 
